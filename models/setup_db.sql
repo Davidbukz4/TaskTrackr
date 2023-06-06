bf816f8533c41d8bba54e95750dd3a7a74104ff8
David Egwuatu <davidegwuatu2@gmail.com>
2023-06-06 18:26:19 +0000

    added db setup file

--- models/setup_db.sql
+++ models/setup_db.sql
@@ -1,6 +1,14 @@
--- Creates user
--- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'username'@'localhost';
-SET PASSWORD FOR 'username'@'localhost' = 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost';
+-- creates the MySQL server user TTR_MYSQL_USER
+
+-- creates database with the name TTR_MYSQL_DB
+CREATE DATABASE IF NOT EXISTS TTR_MYSQL_DB;
+
+-- creates user with the name 'TTR_MYSQL_USER' and password 'TTR_MYSQL_PWD'
+CREATE USER IF NOT EXISTS 'TTR_MYSQL_USER'@'localhost';
+SET PASSWORD FOR 'TTR_MYSQL_USER'@'localhost' = 'TTR_MYSQL_PWD';
+
+-- grant all privileges on database 'TTR_MYSQL_DB' to user 'hbnb_dev'
+GRANT ALL PRIVILEGES ON `TTR_MYSQL_DB`.* TO 'TTR_MYSQL_USER'@'localhost';
+
+-- apply changes to database
 FLUSH PRIVILEGES;
